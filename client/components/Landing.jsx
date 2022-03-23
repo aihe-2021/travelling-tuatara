837f547dd2b013f6b3a439e3ee7137939b079ece
dane-williams <uhtiger@gmail.com>
2022-03-23 15:04:49 +1300

    background landing page done

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -3,13 +3,26 @@ import React from 'react'
 function Landing () {
   return (
     <>
-      <div className="background">
-        <img className="banner" src='/images/tt-banner.jpeg'/>
+      <div className="center">
+        <img className="center_image" src='/images/tt-banner.jpeg'/>
       </div>
-      <div className='lower'>
-        <div><img className='link-images' src='/images/header.jpeg'/></div>
-        <div><img className='link-images' src='/images/tt-logo.jpeg'/></div>
-        <div><img className='link-images' src='/images/art.jpeg'/></div>
+      <div className='background'>
+        <div className='background_div1'>
+          <img className='background_image' src='/images/header.jpeg'/>
+          <div className="Link_Text"><h1>Centered</h1></div>
+        </div>
+        <div className='background_div2'>
+          <img className='background_image' src='/images/tt-logo.jpeg'/>
+          <div className="Link_Text"><h1>Centered</h1></div>
+        </div>
+        <div className='background_div3'>
+          <img className='background_image' src='/images/art.jpeg'/>
+          <div className="Link_Text"><h1>Centered</h1></div>
+        </div>
+        <div className='background_div4'>
+          <img className='background_image' src='/images/forest.jpeg'/>
+          <div className="Link_Text"><h1>Centered</h1></div>
+        </div>
       </div>
     </>
   )
