bef9ae55d3b1026fd44da8d1d5c28483e4cabe9d
dane-williams <uhtiger@gmail.com>
2022-03-28 22:07:44 +1300

    add personal pages

--- client/components/Brian.jsx
+++ client/components/Brian.jsx
@@ -4,16 +4,22 @@ function Brian () {
   return (
     <>
       <div className='header'>
-        <h1>The Travelling Tuatara</h1>
+        <img className='banner-image' src='/images/sunset-lake.jpg'/>
+        <div className='centered'><h1>The Travelling Tuatara</h1></div>
       </div>
       <div className="brian">
         <div className="content">
           <div className="content-overlay"></div>
-          <img className='image-brian' src='/images/brian-performing.jpeg'/>
+          <img className='image-brian' src='/images/brian-trees.JPG'/>
           <div className="content-details fadeIn-bottom">
             <h1 className="content-title">Brian Potiki</h1>
           </div>
         </div>
+      </div>
+      <div className='brian-about'>
+        <h1>Brian Potiki</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
       </div>
     </>
   )
--- client/components/Jill.jsx
+++ client/components/Jill.jsx
@@ -5,16 +5,22 @@ function Jill () {
     <>
 
       <div className='header'>
-        <h1>The Travelling Tuatara</h1>
+        <img className='banner-image' src='/images/banner.jpeg'/>
+        <div className='centered'><h1>The Travelling Tuatara</h1></div>
       </div>
       <div className="jill">
         <div className="content">
           <div className="content-overlay"></div>
-          <img className='image-jill' src='/images/jill-singing.jpeg'/>
+          <img className='image-jill' src='/images/jill.jpeg'/>
           <div className="content-details fadeIn-bottom">
             <h1 className="content-title">Jill Walker</h1>
           </div>
         </div>
+      </div>
+      <div className='jill-about'>
+        <h1>Jill Walker</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
       </div>
 
     </>
